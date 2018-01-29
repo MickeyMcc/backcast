a68646065d4ad6a2dd108e32350fe622e6c973ac
mickeymcc <mccmarci@gmail.com>
2018-01-29 15:29:14 -0600

    Passes 'should render five 'videoListEntry' when given five videos' test

--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,10 +1,19 @@
 var VideoListView = Backbone.View.extend({
-
+  init: function() {
+    this.collection.on('add', this.render, this);
+  },
 
   render: function() {
+    console.log('VideoListView is rendering');
     this.$el.children().detach();
+    this.collection.forEach(this.renderEntry, this);
     this.$el.html(this.template());
     return this;
+  },
+
+  renderEntry: function(entry) {
+    var listEntry = new VideoListEntryView({model: entry});
+    this.$el.append(listEntry.render());
   },
 
   template: templateURL('src/templates/videoList.html')
